828131bb86235bad01a856bf1c8ebb85e9bd3dc6
tzmfreedom <makoto_tajitsu@hotmail.co.jp>
2019-05-03 09:59:15 +0900

    update

--- main.go
+++ main.go
@@ -7,15 +7,17 @@ import (
 	"github.com/k0kubun/pp"
 	"image/color"
 	"log"
+	"math"
 	"math/rand"
 	"strconv"
 )
 
 var timer int
-var x, y int
+var x, y float64
 var score int
 var phase int
-var initPlayerX, initPlayerY int
+var initPlayerX, initPlayerY float64
+var speed float64
 
 const (
 	screenWidth = 320
@@ -29,17 +31,34 @@ const (
 )
 
 type Enemy struct {
-	X int
-	Y int
-	Vx int
-	Vy int
-	SizeX int
-	SizeY int
-	Counter int
-	VCounter int
+	Type int
+	Id int
+	X float64
+	Y float64
+	Vx float64
+	Vy float64
+	SizeX float64
+	SizeY float64
+	Score int
+	HP int
 }
 
+type Shot struct {
+	Id int
+	X float64
+	Y float64
+	Vx float64
+	Vy float64
+	SizeX float64
+	SizeY float64
+	Power int
+	MoveCount int
+}
+
+var shotIndex = 1
 var enemies = []*Enemy{}
+var shots = []*Shot{}
+var enemyShots = []*Shot{}
 
 func update(screen *ebiten.Image) error {
 	if phase != PHASE_GAMEOVER {
@@ -52,6 +71,7 @@ func update(screen *ebiten.Image) error {
 		ebitenutil.DebugPrint(screen, strconv.Itoa(score))
 
 		emenyAction()
+		moveShot()
 	}
 	if ebiten.IsDrawingSkipped() {
 		return nil
@@ -68,37 +88,124 @@ func main() {
 
 func emenyAction() {
 	for _, enemy := range enemies {
-		enemy.Counter++
-		if enemy.Counter == 1 {
-			enemy.X += enemy.Vx
-			enemy.Y += enemy.Vy
-			enemy.Counter = 0
+		enemy.X += enemy.Vx
+		enemy.Y += enemy.Vy
+
+		if enemy.Type == 1 && timer % 60 == 0 {
+			v := math.Sqrt(math.Pow((x - enemy.X), 2) + math.Pow((y - enemy.Y), 2))
+			enemyShots = append(enemyShots, &Shot{
+				Id: shotIndex,
+				X: enemy.X,
+				Y: enemy.Y,
+				Vx: (x - enemy.X)*3/v,
+				Vy: (y - enemy.Y)*3/v,
+				SizeX: 2,
+				SizeY: 2,
+				MoveCount: 4,
+			})
+			shotIndex++
 		}
 	}
 }
 
+func moveShot() {
+	for _, shot := range enemyShots {
+		if timer % shot.MoveCount == 0 {
+			shot.X += shot.Vx
+			shot.Y += shot.Vy
+		}
+		if conflictPlayer(shot) {
+			phase = PHASE_GAMEOVER
+			return
+		}
+		if shot.X < 0 || shot.Y < 0 || shot.X > screenWidth || shot.Y > screenHeight {
+			removeEnemyShot(shot)
+		}
+	}
+	for _, shot := range shots {
+		shot.X += shot.Vx
+		shot.Y += shot.Vy
+		enemy := conflictEnemy(shot.X, shot.Y, shot.SizeX, shot.SizeY)
+		if enemy != nil {
+			enemy.HP -= shot.Power
+			if enemy.HP <= 0 {
+				score += enemy.Score
+				removeEnemy(enemy)
+				removeShot(shot)
+			}
+		}
+		if shot.X < 0 || shot.Y < 0 || shot.X > screenWidth || shot.Y > screenHeight {
+			removeShot(shot)
+		}
+	}
+}
+
+func removeEnemy(search *Enemy) {
+	newEnemies := []*Enemy{}
+	for _, e := range enemies {
+		if e.Id != search.Id {
+			newEnemies = append(newEnemies, e)
+		}
+	}
+	enemies = newEnemies
+}
+
+func removeShot(search *Shot) {
+	newShots := []*Shot{}
+	for _, s := range shots {
+		if s.Id != search.Id {
+			newShots = append(newShots, s)
+		}
+	}
+	shots = newShots
+}
+
+func removeEnemyShot(search *Shot) {
+	newShots := []*Shot{}
+	for _, s := range enemyShots {
+		if s.Id != search.Id {
+			newShots = append(newShots, s)
+		}
+	}
+	enemyShots = newShots
+}
+
+
 func handleInput() {
-	if v := inpututil.KeyPressDuration(ebiten.KeyLeft); v > 0 && v%2 == 0 {
+	if v := inpututil.KeyPressDuration(ebiten.KeyLeft); v > 0 {
 		if x > 0 {
-			x-=5
+			x-=speed
 		}
 	}
-	if v := inpututil.KeyPressDuration(ebiten.KeyRight); v > 0 && v%2 == 0 {
+	if v := inpututil.KeyPressDuration(ebiten.KeyRight); v > 0 {
 		if x < screenWidth {
-			x += 5
+			x += speed
 		}
 	}
-	if v := inpututil.KeyPressDuration(ebiten.KeyDown); v > 0 && v%2 == 0 {
+	if v := inpututil.KeyPressDuration(ebiten.KeyDown); v > 0 {
 		if y < screenHeight {
-			y += 5
+			y += speed
 		}
 	}
-	if v := inpututil.KeyPressDuration(ebiten.KeyUp); v > 0 && v%2 == 0 {
+	if v := inpututil.KeyPressDuration(ebiten.KeyUp); v > 0 {
 		if y > 0 {
-			y -= 5
+			y -= speed
 		}
 	}
-	if isConflict() {
+	if v := inpututil.KeyPressDuration(ebiten.KeyS); v == 1 || (v > 0 && v%10 == 0) {
+		shots = append(shots, &Shot{
+			Id: shotIndex,
+			X: x+5,
+			Y: y+5,
+			SizeX: 5,
+			SizeY: 5,
+			Vx: 1,
+			Vy: 0,
+			Power: 10,
+		})
+		shotIndex++
+	}
+	if conflictEnemy(x, y, playerSize, playerSize) != nil {
 		phase = PHASE_GAMEOVER
 	}
 }
@@ -110,8 +217,24 @@ func draw(screen *ebiten.Image) {
 	options.GeoM.Translate(float64(x), float64(y))
 	screen.DrawImage(img, options)
 
+	for _, shot := range shots {
+		img, _ := ebiten.NewImage(int(shot.SizeX), int(shot.SizeY), 0)
+		img.Fill(color.RGBA{0x00, 0x00, 0xff, 0xff})
+		options := &ebiten.DrawImageOptions{}
+		options.GeoM.Translate(float64(shot.X), float64(shot.Y))
+		screen.DrawImage(img, options)
+	}
+
+	for _, shot := range enemyShots {
+		img, _ := ebiten.NewImage(int(shot.SizeX), int(shot.SizeY), 0)
+		img.Fill(color.RGBA{0xff, 0xff, 0xff, 0xff})
+		options := &ebiten.DrawImageOptions{}
+		options.GeoM.Translate(float64(shot.X), float64(shot.Y))
+		screen.DrawImage(img, options)
+	}
+
 	for _, enemy := range enemies {
-		img, _ := ebiten.NewImage(playerSize, playerSize, 0)
+		img, _ := ebiten.NewImage(int(enemy.SizeX), int(enemy.SizeY), 0)
 		img.Fill(color.RGBA{0xff, 0x00, 0x00, 0xff})
 		options := &ebiten.DrawImageOptions{}
 		options.GeoM.Translate(float64(enemy.X), float64(enemy.Y))
@@ -120,11 +243,18 @@ func draw(screen *ebiten.Image) {
 	// text.Draw(screen, string(score), scoreFont, scoreX, scoreY, color.White)
 }
 
-func isConflict() bool {
+func conflictEnemy(x, y, sizeX, sizeY float64) *Enemy {
 	for _, enemy := range enemies {
-		if enemy.Y < y + playerSize && enemy.Y + enemy.SizeY > y && enemy.X + enemy.SizeX > x && enemy.X < x + playerSize {
-			return true
+		if enemy.Y < y + sizeY && enemy.Y + enemy.SizeY > y && enemy.X + enemy.SizeX > x && enemy.X < x + sizeX {
+			return enemy
 		}
+	}
+	return nil
+}
+
+func conflictPlayer(shot *Shot) bool {
+	if shot.Y < y + playerSize && shot.Y + shot.SizeY > y && shot.X + shot.SizeX > x && shot.X < x + playerSize {
+		return true
 	}
 	return false
 }
@@ -134,17 +264,23 @@ func init() {
 	score = 0
 	x = initPlayerX
 	y = initPlayerY
+	speed = 1
 	phase = PHASE_GAMESTART
 	n := 30 + rand.Intn(5)
 	for i := 0; i < n; i++ {
 		enemy := &Enemy{
-			X: 20 + rand.Intn(300),
-			Y: rand.Intn(240),
-			Vx: -10 + rand.Intn(20),
-			Vy: -10 + rand.Intn(20),
-			SizeX: 5 + rand.Intn(5),
-			SizeY: 5 + rand.Intn(5),
-			Counter: 0,
+			Id: i,
+			X: 20 + float64(rand.Intn(300)),
+			Y: float64(rand.Intn(240)),
+			Vx: 0,
+			Vy: 0,
+			//Vx: -5 + rand.Intn(10),
+			//Vy: -5 + rand.Intn(10),
+			SizeX: 5 + float64(rand.Intn(5)),
+			SizeY: 5 + float64(rand.Intn(5)),
+			Score: 100 + rand.Intn(50),
+			Type: rand.Intn(2),
+			HP: 10 + rand.Intn(100),
 		}
 		enemies = append(enemies, enemy)
 	}
